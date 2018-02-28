2dc4295552819d8f17481e1e96c435f95821c9c9
zhihanzzh <zzhzhangzhihan@yahoo.com>
2018-02-28 01:06:59 -0500

    finished check noePlaying

--- lab3/client/src/App.js
+++ lab3/client/src/App.js
@@ -1,18 +1,62 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Spotify from 'spotify-web-api-js';
+
+const spotifyWebApi = new Spotify()
 
 class App extends Component {
+  constructor() {
+    super()
+    const params = this.getHashParams()
+    this.state = {
+      loggedIn: params.access_token ? true : false,
+      nowPlaying : {
+        name: 'not yet',
+        img: ''
+      }
+    }
+    if (params.access_token){
+      spotifyWebApi.setAccessToken(params.access_token)
+    } else {
+      console.log('false')
+    }
+  }
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    while ( e = r.exec(q)) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+  }
+
+  getNowPlaying() {
+    spotifyWebApi.getMyCurrentPlaybackState()
+    .then((response) => {
+      this.setState({
+        nowPlaying: {
+          name: response.item.name,
+          img: response.item.album.images[0].url
+        }
+      })
+    })
+  }
   render() {
+    
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <a href='http://localhost:8888'>
+        <button className='btn btn-primary'>Login with Spotify </button>
+        </a>
+        <div>now is playing {this.state.nowPlaying.name}</div>
+        <img src={this.state.nowPlaying.img} style={{width:100}} />
+        <button onClick={() => this.getNowPlaying()}> check </button>
       </div>
     );
   }
