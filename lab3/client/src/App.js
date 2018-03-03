72813aa093ee205a5fc87457f7448683bb841cdf
zhihanzzh <zzhzhangzhihan@yahoo.com>
2018-03-03 17:37:41 -0500

    finished

--- lab3/client/src/App.js
+++ lab3/client/src/App.js
@@ -1,41 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import Spotify from 'spotify-web-api-js';
+import TrackList from "./components/TrackList";
 
 const spotifyWebApi = new Spotify()
 
-class TrackList extends Component {
-  render() {
-    console.log(this.props.tracklist)
-    return (
-      <div className="row">
-        {this.props.tracklist.map(track =>
-          <div className="col-sm-12 col-md-6 col-lg-3">
-            <div className="card" id="card1">
-              <div className="card-header">
-                <h1>{track.name}</h1>
-                <a href={track.artists[0].external_urls.spotify}>{track.artists[0].name}</a>
-              </div>
-              <img src={track.album.images[0].url} alt="" className="card-img" />
-              <div className="card-body">
-                <p>popularity:{track.popularity}</p>
-              </div>
-              <div className="card-footer">
-              <p className="card-text">
-              <a href={track.album.external_urls.spotify}>Album Name: {track.album.name}</a>
-              </p>
-                <audio controls src={this.props.tracklist.length > 0 ? track.preview_url : ''}>
-                Your browser does not support the audio element.
-                </audio>
-              </div>
-            </div>
-          </div>
-        )}
 
-      </div>
-    );
-  }
-}
 
 
 
@@ -73,7 +43,7 @@ class App extends Component {
         this.setState({
           tracks: response.tracks.items
         })
-        
+
       })
   }
 
@@ -97,10 +67,10 @@ class App extends Component {
               <TrackList tracklist={this.state.tracks} />
               : ''}
           </div> : <div>
-          <h1>Hi, welcome to this online Spotify Web Api, Please Login with spotify first!</h1>
-          <a href='http://localhost:8888/login'>
-          <button className='btn btn-primary'>Login with Spotify </button>
-        </a>
+            <h1>Hi, welcome to this online Spotify Web Api, Please Login with spotify first!</h1>
+            <a href='http://localhost:8888/login'>
+              <button className='btn btn-primary'>Login with Spotify </button>
+            </a>
           </div>
         }
       </div>
