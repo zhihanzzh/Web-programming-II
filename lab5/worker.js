ce8a6072c7ec612d5ca6b9581c27346aaf82de28
zhihanzzh <zzhzhangzhihan@yahoo.com>
2018-03-22 04:52:53 -0400

    in process

--- lab5/worker.js
+++ lab5/worker.js
@@ -2,3 +2,9 @@ var fs =require('fs')
 const peopleList=JSON.parse(fs.readFileSync('dood.json'));
 
 const redisConnection = require("./redis-connection");
+
+redisConnection.on("get-person:request:*", (message, channel) => {
+    let requestId = message.requestId;
+    let eventName = message.eventName;
+
+});
